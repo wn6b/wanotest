bad0b5fca8b289e116c49ea8f4af280797dec98d
Wano | مروان <waylalyzydy55@gmail.com>
2026-04-22 20:22:53 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// js/core/config.js
+export const SYSTEM_CONFIG = {
+    VERSION: "2026.1.5",
+    CODENAME: "WANO_PRIME",
+    AUTHOR: "WanoDev",
+    API_BASE: "https://api.wanohost.io/v1",
+    FEATURES: {
+        ENCRYPTION: true,
+        AUTO_BACKUP: true,
+        AI_OPTIMIZATION: true
+    }
+};
